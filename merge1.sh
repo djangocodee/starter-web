cfc027b80303e0ceeb4f0e9b737d7a7790ac3403
jayapratap21 <jayapratap21@gmail.com>
2022-02-24 23:13:56 +0530

    all

--- merge1.sh
+++ merge1.sh
@@ -1,7 +1,3 @@
-##
-echo "Enter number of days to see the merged files:"
-var = $n
 
-find
 git log --merges >> mergejp.txt
 	
--- mergetest.sh
+++ mergetest.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
 ## script for merging 
 
-git log --merges >> mergetest.txt
+Echo "Enter number of days to see merged files history: "
+
+read n
+
+git log --since='$n days ago' > mergetest.txt
+echo "git log --since='$n days ago' > mergetest.txt"
 
 echo "Output has saved as mergetest.txt"
 
