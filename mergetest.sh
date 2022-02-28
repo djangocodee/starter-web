f73a415ab925481af1f9a5e455316fd7837eb1f9
jayapratap21 <jayapratap21@gmail.com>
2022-02-28 19:00:34 +0530

    all

--- mergetest.sh
+++ mergetest.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+
 ## script for merging 
 
 Echo "Enter number of days to see merged files history: "
 
 read n
 
-git log --since='$n days ago' > mergetest.txt
+git log --raw --since="$n days ago" --pretty=format:'%cd Author %an' > mergetest.txt
 echo "git log --since='$n days ago' > mergetest.txt"
 
 echo "Output has saved as mergetest.txt"
